2560936b7c3105b30d0291e9b0a159470482a9f6
Elad Levy <162992264+eladlevy42@users.noreply.github.com>
2024-07-22 15:17:37 +0300

    business review

--- src/components/general-components/BussinessReview.tsx
+++ src/components/general-components/BussinessReview.tsx
@@ -1,12 +1,17 @@
 import { Review as ReviewType } from "src/types";
 import StarRating from "./StarRanking";
 import { ThumbsUp } from "lucide-react";
+import { useAuth } from "@/providers/user.context";
 
 interface ReviewProps {
   review: ReviewType;
 }
 
 function Review({ review }: ReviewProps) {
+  const { loggedInUser } = useAuth();
+
+  async function toggleLike(reviewId: string): Promise<void> {}
+
   return (
     <div className="p-4 border rounded-lg shadow mb-4 flex flex-col gap-2">
       <div className=" flex justify-between items-center">
