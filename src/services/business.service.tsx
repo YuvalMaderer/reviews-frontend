ffedd4a4a4dbb47e277828751b79cdaf8dd351dd
omer3110 <omer.sidi10@gmail.com>
2024-07-22 14:04:44 +0300

    search

--- src/services/business.service.tsx
+++ src/services/business.service.tsx
@@ -1,9 +1,9 @@
 import api from "./api.service";
 import { Business } from "../types";
 
-export function getBusinesses() {
-  return api.get<Business[]>("/business");
-}
+export const getBusinesses = (page: number) => {
+  return api.get<{ business: Business[] }>(`/api/business?page=${page}`);
+};
 
 // export function createBusiness(business: Business) {
 //   return api.post<Business>("/business", business);
