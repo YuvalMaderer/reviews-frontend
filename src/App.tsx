6177b1ad2b7418e1c7b8032f90971d2dad82e098
Yuval <yuvalmaderer12@gmail.com>
2024-07-23 13:17:28 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import BusinessPage from "./pages/BusinessPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from "./components/general-components/NavBar";
+import ContactUsPage from "./pages/ContactUsPage";
 
 const App: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/business" element={<BusinessPage />} />
+        <Route path="/contact" element={<ContactUsPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
