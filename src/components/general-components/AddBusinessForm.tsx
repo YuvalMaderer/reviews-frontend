45d509defcacf37b0ea4d1a7397a6873c66ead3a
Elad Levy <162992264+eladlevy42@users.noreply.github.com>
2024-07-23 19:01:05 +0300

    images

--- src/components/general-components/AddBusinessForm.tsx
+++ src/components/general-components/AddBusinessForm.tsx
@@ -1,11 +1,37 @@
+import React from "react";
+import api from "@/services/api.service";
+
 function AddBusinessForm() {
+  async function createBusiness(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.currentTarget);
+    try {
+      const response = await api.post("/business", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      console.log("Business created successfully:", response.data);
+    } catch (error) {
+      console.error("Error creating business:", error);
+    }
+  }
+
   return (
     <div className="flex flex-col">
-      <input type="text" placeholder="name" />
-      <input type="text" placeholder="category" />
-      <input type="text" placeholder="description" />
-      <input type="file" placeholder="name" />
-      <button>Submit</button>
+      <form onSubmit={createBusiness}>
+        <input type="text" name="name" placeholder="name" required />
+        <input type="text" name="category" placeholder="category" required />
+        <input
+          type="text"
+          name="description"
+          placeholder="description"
+          required
+        />
+        <input type="file" name="image" placeholder="image" required />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
